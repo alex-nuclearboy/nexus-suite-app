2a3bba3011f21ef64fe25acba4917daf7e683e1e
Aleksander <alex.nuclearboy@gmail.com>
2024-07-05 14:42:12 +0200

    Replace the secret key with an environment variable

--- nexus_suite/settings.py
+++ nexus_suite/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+# Load environment variables from the .env file
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,9 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = (
-    'django-insecure-syzzv)hybp8+)0+)_i7pja8nc0ym9+y3nf2uk5vj&lzal!sd$k'
-)
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
