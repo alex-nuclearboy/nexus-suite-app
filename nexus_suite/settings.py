c506cdde4204701ead45ef613635affbf8a6a340
Aleksander <alex.nuclearboy@gmail.com>
2024-07-05 17:12:26 +0200

    Delete an unnecessary package

--- nexus_suite/settings.py
+++ nexus_suite/settings.py
@@ -13,7 +13,6 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 import os
 from pathlib import Path
 from dotenv import load_dotenv
-import dj_database_url
 
 # Load environment variables from the .env file
 load_dotenv()
