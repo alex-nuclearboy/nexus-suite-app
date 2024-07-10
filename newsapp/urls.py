e62e0096956c3483864e1d0e47de3de96f1e9623
Aleksander <alex.nuclearboy@gmail.com>
2024-07-10 22:11:22 +0200

    Add new routes

--- newsapp/urls.py
+++ newsapp/urls.py
@@ -5,5 +5,9 @@ app_name = "newsapp"
 
 urlpatterns = [
     path('', views.main, name='index'),
+    path('news/<str:category>/', views.news_by_category, name='news_by_category'),
+    path('exchange-rates/', views.exchange_rates_page, name='exchange_rates'),
+    path('convert-currency/', views.convert_currency_view, name='convert_currency'),
+    path('weather/', views.weather_page, name='weather'),
     path('set_timezone/', views.set_timezone, name='set_timezone'),
 ]
