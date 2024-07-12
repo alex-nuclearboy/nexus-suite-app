355e2154f5b002aa07b90da4157475ae26a96887
Aleksander <alex.nuclearboy@gmail.com>
2024-07-12 22:30:49 +0200

    Add custom filter for 24h time format

--- newsapp/templatetags/custom_filters.py
+++ newsapp/templatetags/custom_filters.py
@@ -1,4 +1,5 @@
 from django import template
+from datetime import datetime
 from ..utils.utils import format_time
 
 register = template.Library()
@@ -14,3 +15,18 @@ def get_item(dictionary, key):
 @register.filter
 def format_time_filter(date_string, lang='en', format_type='abbr'):
     return format_time(date_string, lang, format_type)
+
+
+@register.filter
+def format_time_24h(date_string):
+    """
+    Converts time from 12-hour AM/PM format to 24-hour format.
+    """
+    # Parse the date_string into a datetime object
+    try:
+        # Assuming the time is in "%I:%M %p" format, e.g., "05:01 PM"
+        dt = datetime.strptime(date_string, "%I:%M %p")
+        # Return time in 24-hour format
+        return dt.strftime("%H:%M")
+    except ValueError:
+        return date_string  # Return original if there is an error
