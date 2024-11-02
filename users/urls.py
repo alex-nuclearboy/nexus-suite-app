074f07a3592633ddfc5e7483e934e0a076839151
Aleksander <alex.nuclearboy@gmail.com>
2024-11-02 14:52:26 +0100

    Update the route

--- users/urls.py
+++ users/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = 'users'
 
 urlpatterns = [
-    path('signup/', views.signupuser, name='signup'),
+    path('signup/', views.SignupUserView.as_view(), name='signup'),
     # path('login/', views.loginuser, name='login'),
     # path('logout/', views.logoutuser, name='logout'),
     # path('profile/', views.profile, name='profile'),
