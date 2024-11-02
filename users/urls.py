fefa58d7dd832bd2132d5638b12a2e6e18f0a859
Aleksander <alex.nuclearboy@gmail.com>
2024-11-02 13:15:29 +0100

    Comment unnecessary routes

--- users/urls.py
+++ users/urls.py
@@ -5,7 +5,7 @@ app_name = 'users'
 
 urlpatterns = [
     path('signup/', views.signupuser, name='signup'),
-    path('login/', views.loginuser, name='login'),
-    path('logout/', views.logoutuser, name='logout'),
-    path('profile/', views.profile, name='profile'),
+    # path('login/', views.loginuser, name='login'),
+    # path('logout/', views.logoutuser, name='logout'),
+    # path('profile/', views.profile, name='profile'),
 ]
