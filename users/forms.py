b8e6bcbb749843b0161975941e010d42898f91d5
Aleksander <alex.nuclearboy@gmail.com>
2024-11-04 14:58:46 +0100

    Add a form for sign-in

--- users/forms.py
+++ users/forms.py
@@ -1,6 +1,7 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
 
 from .utils.translations import translations
 
@@ -56,3 +57,50 @@ class RegisterForm(UserCreationForm):
             if email and User.objects.filter(email=email).exists():
                 raise forms.ValidationError(self.transl["email_exists"])
             return email
+
+        def clean_password2(self):
+            password1 = self.cleaned_data.get('password1')
+            password2 = self.cleaned_data.get('password2')
+            if password1 and password2 and password1 != password2:
+                raise forms.ValidationError(
+                    self.error_messages['password_mismatch']
+                )
+            return password2
+
+
+class LoginForm(AuthenticationForm):
+    username_or_email = forms.CharField(max_length=100, required=True,
+                                        widget=forms.TextInput(attrs={
+                                            'placeholder': '',
+                                        }))
+    password = forms.CharField(max_length=50, required=True,
+                               widget=forms.PasswordInput(attrs={
+                                   'placeholder': '',
+                               }))
+
+    class Meta:
+        fields = ['username_or_email', 'password']
+
+    def __init__(self, *args, **kwargs):
+        self.lan = kwargs.pop('language', 'en')
+        self.transl = translations.get(self.lan, translations['en'])
+
+        super(LoginForm, self).__init__(*args, **kwargs)
+
+        self.fields['username_or_email'].widget.attrs.update({
+            'placeholder': self.transl['enter_username_or_email'],
+        })
+        self.fields['password'].widget.attrs.update({
+            'placeholder': self.transl['enter_password'],
+        })
+
+    def clean(self):
+        cleaned_data = super().clean()
+        username_or_email = cleaned_data.get('username_or_email')
+        password = cleaned_data.get('password')
+
+        if username_or_email and password:
+            user = authenticate(username=username_or_email, password=password)
+            if user is None:
+                raise forms.ValidationError(self.transl["invalid_credentials"])
+        return cleaned_data
