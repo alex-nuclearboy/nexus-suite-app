8877487a17cc43924aa730bf9e6d8b210ee79d35
Aleksander <alex.nuclearboy@gmail.com>
2024-11-05 17:37:18 +0100

    Add documentation

--- users/models.py
+++ users/models.py
@@ -8,6 +8,16 @@ from io import BytesIO
 
 
 class Profile(models.Model):
+    """
+    A model representing a user profile.
+
+    Parameters:
+        user (OneToOneField): A one-to-one relationship with the User model.
+        first_name (CharField): The user's first name, optional.
+        last_name (CharField): The user's last name, optional.
+        avatar (CloudinaryField): The user's avatar image stored in Cloudinary,
+                                  with a default value if none is provided.
+    """
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     first_name = models.CharField(max_length=30, blank=True, null=True)
     last_name = models.CharField(max_length=30, blank=True, null=True)
@@ -18,9 +28,26 @@ class Profile(models.Model):
     )
 
     def __str__(self):
+        """
+        Returns a string representation of the Profile instance.
+
+        Returns:
+            str: A string containing the user's full name and username.
+        """
         return f"{self.first_name} {self.last_name} ({self.user.username})"
 
     def save(self, *args, **kwargs):
+        """
+        Saves the Profile instance, resizing the avatar image if necessary.
+
+        This method overrides the default save method to ensure that the
+        avatar image is resized to fit within a 250x250 pixel bounding box
+        if it exceeds those dimensions.
+
+        Parameters:
+            *args: Variable length argument list.
+            **kwargs: Arbitrary keyword arguments.
+        """
         if self.avatar and not self._state.adding:
             # Save the instance before resizing
             super().save(*args, **kwargs)
