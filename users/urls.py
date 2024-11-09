8338bd69f6fb8824afedb1d986d03636987de0ad
Aleksander <alex.nuclearboy@gmail.com>
2024-11-09 16:30:14 +0100

    Add a route to log out

--- users/urls.py
+++ users/urls.py
@@ -6,6 +6,6 @@ app_name = 'users'
 urlpatterns = [
     path('signup/', views.SignupUserView.as_view(), name='signup'),
     path('login/', views.LoginUserView.as_view(), name='login'),
-    # path('logout/', views.logoutuser, name='logout'),
+    path('logout/', views.LogoutUserView.as_view(), name='logout'),
     # path('profile/', views.profile, name='profile'),
 ]
