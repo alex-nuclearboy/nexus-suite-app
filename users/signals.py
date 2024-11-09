19d912b0b66c30f88c837b6e3bef264f7968ad7f
Aleksander <alex.nuclearboy@gmail.com>
2024-11-09 16:29:23 +0100

    Optimisation of signals

--- users/signals.py
+++ users/signals.py
@@ -6,11 +6,40 @@ from .models import Profile
 
 
 @receiver(post_save, sender=User)
-def create_profile(sender, instance, created, **kwargs):
+def create_or_update_user_profile(sender, instance, created, **kwargs):
+    """
+    Creates or updates a user's profile when a user is saved.
+
+    Parameters:
+        sender (Model): The model that sent the signal (User).
+        instance (User): The instance of the model (the user being saved).
+        created (bool): Boolean flag indicating whether the user was created.
+        **kwargs: Arbitrary keyword arguments.
+
+    This signal performs two operations:
+        - If a new user is created, it creates a new Profile
+          associated with the user.
+        - If the user is updated (but not created),
+          it updates the associated Profile's first and last name.
+    """
     if created:
-        Profile.objects.create(user=instance)
-
-
-@receiver(post_save, sender=User)
-def save_profile(sender, instance, **kwargs):
-    instance.profile.save()
+        # Create a new profile when a new user is created
+        Profile.objects.create(
+            user=instance,
+            first_name=instance.first_name,
+            last_name=instance.last_name
+        )
+    else:
+        # Update existing profile when user details change
+        try:
+            profile = instance.profile
+        except Profile.DoesNotExist:
+            # If profile does not exist, create one
+            profile = Profile.objects.create(
+                user=instance,
+                first_name=instance.first_name,
+                last_name=instance.last_name
+            )
+        profile.first_name = instance.first_name
+        profile.last_name = instance.last_name
+        profile.save()
