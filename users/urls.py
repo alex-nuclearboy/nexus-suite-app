cb6bb7eee2ec722ce8bae0a37b278f3984e58cd8
Aleksander <alex.nuclearboy@gmail.com>
2024-11-12 16:57:30 +0100

    Add a route for the profile page

--- users/urls.py
+++ users/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('signup/', views.SignupUserView.as_view(), name='signup'),
     path('login/', views.LoginUserView.as_view(), name='login'),
     path('logout/', views.LogoutUserView.as_view(), name='logout'),
-    # path('profile/', views.profile, name='profile'),
+    path('profile/', views.UpdateUserProfileView.as_view(), name='profile'),
 ]
