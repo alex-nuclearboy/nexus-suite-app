ecc8166ceabbc1cdea2fc38c3cd05428f12f2855
Aleksander <alex.nuclearboy@gmail.com>
2024-11-12 22:46:44 +0100

    Transfer an email to a user's profile

--- users/signals.py
+++ users/signals.py
@@ -10,24 +10,29 @@ def create_or_update_user_profile(sender, instance, created, **kwargs):
     """
     Creates or updates a user's profile when a user is saved.
 
-    Parameters:
-        sender (Model): The model that sent the signal (User).
-        instance (User): The instance of the model (the user being saved).
-        created (bool): Boolean flag indicating whether the user was created.
-        **kwargs: Arbitrary keyword arguments.
-
     This signal performs two operations:
-        - If a new user is created, it creates a new Profile
-          associated with the user.
-        - If the user is updated (but not created),
-          it updates the associated Profile's first and last name.
+        - If a new user is created, a new Profile is created and associated
+          with the user.
+        - If the user is updated (but not created), the associated Profile's
+          first name, last name, and email are updated.
+
+    :param sender: The model that sent the signal (User).
+    :type sender: Model
+    :param instance: The instance of the model (the user being saved).
+    :type instance: User
+    :param created: Boolean flag indicating whether the user was created.
+    :type created: bool
+    :param kwargs: Arbitrary keyword arguments.
+    :type kwargs: dict
+    :return: None
     """
     if created:
         # Create a new profile when a new user is created
         Profile.objects.create(
             user=instance,
             first_name=instance.first_name,
-            last_name=instance.last_name
+            last_name=instance.last_name,
+            email=instance.email
         )
     else:
         # Update existing profile when user details change
@@ -38,8 +43,12 @@ def create_or_update_user_profile(sender, instance, created, **kwargs):
             profile = Profile.objects.create(
                 user=instance,
                 first_name=instance.first_name,
-                last_name=instance.last_name
+                last_name=instance.last_name,
+                email=instance.email
             )
+
+        # Update profile fields if the user details have changed
         profile.first_name = instance.first_name
         profile.last_name = instance.last_name
+        instance.profile.email = instance.email
         profile.save()
