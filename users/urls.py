49561e0f58d4d311e66d6e1050f4404b58197b74
Aleksander <alex.nuclearboy@gmail.com>
2024-11-22 21:31:35 +0100

    Update routes

--- users/urls.py
+++ users/urls.py
@@ -8,4 +8,24 @@ urlpatterns = [
     path('login/', views.LoginUserView.as_view(), name='login'),
     path('logout/', views.LogoutUserView.as_view(), name='logout'),
     path('profile/', views.UpdateUserProfileView.as_view(), name='profile'),
+    path(
+        'password_reset/',
+        views.CustomPasswordResetRequestView.as_view(),
+        name='password_reset'
+    ),
+    path(
+        'password_reset/done/',
+        views.CustomPasswordResetDoneView.as_view(),
+        name='password_reset_done'
+    ),
+    path(
+        'password/reset/<uidb64>/<token>/',
+        views.CustomPasswordResetConfirmView.as_view(),
+        name='password_reset_confirm'
+    ),
+    path(
+        'password/reset/complete/',
+        views.CustomPasswordResetCompleteView.as_view(),
+        name='password_reset_complete'
+    ),
 ]
