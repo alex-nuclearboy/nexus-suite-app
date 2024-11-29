c401d127908d9d7858a24a5c09e8370a2e6fb5bb
Aleksander <alex.nuclearboy@gmail.com>
2024-11-29 11:50:34 +0100

    Add an administrative interface for models

--- quotesapp/admin.py
+++ quotesapp/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import Author, Quote, Tag
 
-# Register your models here.
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('fullname', 'born_date', 'born_location', 'description')
+
+
+@admin.register(Quote)
+class QuoteAdmin(admin.ModelAdmin):
+    list_display = ('quote', 'author', 'tags_display')
+
+    # Show tags in the list of quotes
+    def tags_display(self, obj):
+        return ', '.join(tag.name for tag in obj.tags)
+    tags_display.short_description = 'Tags'
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('name',)
